53e69aead401f10b4d6d7f40c0050f4e81f01d53
Akash Yadav <91262354+akash701752@users.noreply.github.com>
2022-01-03 10:47:34 +0530

    Add files via upload

---
+++ t3P15.cpp
@@ -0,0 +1,22 @@
+/*  Practical 15
+    Que 15.3 : IO Manipulators
+    Task     : Use anipulators
+*/ 
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	cout<<"\t\t Akash Yadav 200111076 A "<<endl ;
+	int n ;
+	cout<<"Entera Integer : ";
+	cin>>n ;
+	cout<<"dec  : "<<dec<<n <<endl ;
+	cout<<"hex  : "<<hex<<n <<endl ;
+	string name = "Practical Task" ;
+    return 0;
+}
